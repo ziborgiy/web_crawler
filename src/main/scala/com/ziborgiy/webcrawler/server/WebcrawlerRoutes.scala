261e4ca025c351c5100d13838ee603a4037f3962
Ziborgiy <ziborgiy@yandex.ru>
2020-08-09 23:45:17 +0300

    Returned routes

--- src/main/scala/com/ziborgiy/webcrawler/server/WebcrawlerRoutes.scala
+++ src/main/scala/com/ziborgiy/webcrawler/server/WebcrawlerRoutes.scala
@@ -1,30 +1,25 @@
 package com.ziborgiy.webcrawler.server
 
-import cats.Parallel
-import cats.data.NonEmptyList
-import cats.effect.{Concurrent, Sync}
+import cats.effect.Sync
 import cats.implicits._
-import com.ziborgiy.webcrawler.model.Urls
 import com.ziborgiy.webcrawler.model.Codecs._
+import com.ziborgiy.webcrawler.model.Urls
 import com.ziborgiy.webcrawler.operations.Titles
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
-import cats.effect.implicits.toConcurrentOps
 
 
 object WebcrawlerRoutes {
 
-  def crawlerRoutes[F[_]: Sync : Concurrent : Parallel](T: Titles[F]): HttpRoutes[F] = {
+  def crawlerRoutes[F[_]: Sync](T: Titles[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
     HttpRoutes.of[F] {
       case rq@POST -> Root / "getTitles" =>
         for {
           req <- rq.as[Urls]
-          ios <- T.getTitles(req.urls)
-          fibers <- NonEmptyList.fromList(ios).getOrElse(None.orNull).parTraverse(_.start)
-          results <- fibers.parTraverse(_.join)
-          resp <- Ok(results.toList.toMap)
+          titles <- T.getTitles(req.urls)
+          resp <- Ok(titles)
         } yield resp
     }
   }
