0f4168187c07f80deb94ff8a07de7f64253113c1
Ziborgiy <ziborgiy@yandex.ru>
2020-08-09 23:29:46 +0300

    Added correct parallel traverse by list with IO's

--- src/main/scala/com/ziborgiy/webcrawler/operations/Titles.scala
+++ src/main/scala/com/ziborgiy/webcrawler/operations/Titles.scala
@@ -1,5 +1,7 @@
 package com.ziborgiy.webcrawler.operations
 
+import cats.Parallel
+import cats.data.NonEmptyList
 import cats.effect.{Concurrent, Sync}
 import cats.implicits._
 import org.jsoup.Jsoup
@@ -7,25 +9,28 @@ import org.jsoup.Jsoup
 import scala.util.Try
 
 trait Titles[F[_]] {
-  def getTitles(urls: Vector[String]): F[List[F[(String, String)]]]
+  def getTitles(urls: Vector[String]): F[Map[String, String]]
 }
 
 object Titles {
 
-  def impl[F[_] : Sync : Concurrent]: Titles[F] = new Titles[F] {
+  def impl[F[_] : Sync : Parallel](implicit c: Concurrent[F]): Titles[F] = new Titles[F] {
 
-    def getTitles(urls: Vector[String]) = {
-      urls.toList.map(getTitle).pure[F]
+    def getTitles(urls: Vector[String]): F[Map[String, String]] = {
+      for {
+        nel <- NonEmptyList.fromList(urls.toSet.toList).getOrElse(None.orNull).parTraverse(getTitle)
+      } yield {
+        nel.toList.toMap
+      }
     }
 
-    def getTitle(url: String) = {
-      Try(Jsoup.connect(url).get).toEither match {
+    def getTitle(url: String): F[(String, String)] = c.delay {
+      Try(Jsoup.connect(url).ignoreHttpErrors(true).get).toEither match {
         case Left(value) => url -> s"${value.getMessage}"
         case Right(value) => url -> value.title()
       }
-    }.pure[F]
+    }
   }
 }
 
-//
 
