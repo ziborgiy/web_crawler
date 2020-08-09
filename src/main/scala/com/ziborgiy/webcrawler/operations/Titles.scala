df80e90e85ad80d29c93d3816406677876261b2b
Ziborgiy <ziborgiy@yandex.ru>
2020-08-09 15:07:34 +0300

    Continue experiments

--- src/main/scala/com/ziborgiy/webcrawler/operations/Titles.scala
+++ src/main/scala/com/ziborgiy/webcrawler/operations/Titles.scala
@@ -1,34 +1,31 @@
 package com.ziborgiy.webcrawler.operations
 
-import cats.Parallel
-import cats.data.NonEmptyList
-import cats.effect.Sync
+import cats.effect.{Concurrent, Sync}
 import cats.implicits._
 import org.jsoup.Jsoup
 
 import scala.util.Try
 
 trait Titles[F[_]] {
-  def getTitles(urls: Vector[String]): F[Map[String, String]]
+  def getTitles(urls: Vector[String]): F[List[F[(String, String)]]]
 }
 
 object Titles {
 
-  def impl[F[_] : Sync : Parallel]: Titles[F] = new Titles[F] {
+  def impl[F[_] : Sync : Concurrent]: Titles[F] = new Titles[F] {
 
-    def getTitles(urls: Vector[String]): F[Map[String, String]] = {
-      for {
-        nel <- NonEmptyList.fromList(urls.toSet.toList).getOrElse(None.orNull).parTraverse(getTitle(_).pure[F])
-      } yield {
-        nel.toList.toMap
-      }
+    def getTitles(urls: Vector[String]) = {
+      urls.toList.map(getTitle).pure[F]
     }
 
-    def getTitle(url: String): (String, String) = {
+    def getTitle(url: String) = {
       Try(Jsoup.connect(url).get).toEither match {
         case Left(value) => url -> s"${value.getMessage}"
         case Right(value) => url -> value.title()
       }
-    }
+    }.pure[F]
   }
 }
+
+//
+
